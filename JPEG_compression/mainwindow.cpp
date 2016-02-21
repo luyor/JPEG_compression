609d621e0a39c8c6efccc699dffe057e3bd4ad31
luyor <807339657@qq.com>
2016-02-20 16:56:14 -0800

    add panels

--- JPEG_compression/mainwindow.cpp
+++ JPEG_compression/mainwindow.cpp
@@ -2,6 +2,7 @@
 #include "ui_mainwindow.h"
 #include <QFileDialog>
 #include <QTextStream>
+#include <iostream>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -20,31 +21,36 @@ void MainWindow::on_open_btn_clicked()
     QString fileName = QFileDialog::getOpenFileName(this,
       tr("Open Image"), "/home/jana", tr("Image Files (*.png *.jpg *.bmp)"));
     QImage image;
-    if (!image.load(fileName)) QDialog::tr("cannot read image file");
-    //QTextStream(stdout) <<fileName<<endl;
+    if (fileName.isEmpty()||!image.load(fileName)) {
+        QDialog::tr("cannot read image file");
+        return;
+    }
     jimage.ChangeImage(image);
-    jimage.UpdateImage();
+    //jimage.UpdateImage();
     
-    QLabel *gv=findChild<QLabel *>("origin");
-    gv->setPixmap(QPixmap::fromImage(jimage.DCT_Y));
+    QLabel *ql=findChild<QLabel *>("origin");
+    ql->setPixmap(QPixmap::fromImage(jimage.origin));
 }
 
 void MainWindow::on_nonuniform_clicked()
 {
-    
+    jimage.ChangeConst(false);
 }
 
 void MainWindow::on_submit_btn_clicked()
 {
-    
+    jimage.UpdateImage();
+    QLabel *ql=findChild<QLabel *>("decoded");
+    ql->setPixmap(QPixmap::fromImage(jimage.decoded));
 }
 
 void MainWindow::on_spinBox_editingFinished()
 {
-    
+    QSpinBox *qsb=findChild<QSpinBox *>("quality");
+    jimage.ChangeQuality(qsb->value());
 }
 
 void MainWindow::on_constant_clicked()
 {
-    
+    jimage.ChangeConst(true);
 }
